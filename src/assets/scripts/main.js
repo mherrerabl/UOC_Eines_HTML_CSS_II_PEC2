e1adcf641b1d6338361b43bc0aea217a39f06690
Míriam Herrera Blanes <mherrerabl@uoc.edu>
2023-12-03 10:27:01 +0100

    v3 article, form

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -13,64 +13,85 @@ import { createApp } from 'vue';
  * Import components
  */
 import Calendar from './../../views/about/Calendar.vue';
+import Tournament from './../../views/about/Tournament.vue';
 
 /**
  * Write any other JavaScript below
  */
 
 +( function() {
+  /**
+   * Nav
+   */
   let location = window.location.pathname.slice(1, window.location.pathname.length);
 
   if (location.length > 0) {
     location = location.slice(0, location.length-5);
   }
 
-  /**
-   * Nav active link
-   */
   $('.nav__link-about').removeAttr('tabindex');
   $('.nav__link').removeClass('active');
 
-    switch (location) {
-      case 'about':
-        $('.nav__link-about').attr('tabindex', -1);
-        $('.nav__link-about').addClass('active');
-        break;
-      case 'players':
-        $('.nav__link-players').attr('tabindex', -1);
-        $('.nav__link-players').addClass('active');
-        break;
-      case 'inscription':
-        $('.nav__link-inscription').attr('tabindex', -1);
-        $('.nav__link-inscription').addClass('active');
-        break;
-      case 'bibliography':
-        $('.nav__link-bibliography').attr('tabindex', -1);
-        $('.nav__link-bibliography').addClass('active');
-        break;
-      default:
-        $('.nav__link-home').attr('tabindex', -1);
-        $('.nav__link-brand').attr('tabindex', -1);
-        $('.nav__link-home').addClass('active');
-        $('.nav__link-brand').addClass('active');
-        break;
-    }
-
-    /* Article */ 
-    if($('.about').length) {
-      //Map
-      createMap('map-location', 41.750306229121676, 1.8436313269920916, '', '', 'Club de tenis', 15);
-
-      //Calendar
-      createApp(Calendar).use().mount('#calendar');
-    }
+  switch (location) {
+    case 'about':
+      $('.nav__link-about').attr('tabindex', -1);
+      $('.nav__link-about').addClass('active');
+      break;
+    case 'players':
+      $('.nav__link-players').attr('tabindex', -1);
+      $('.nav__link-players').addClass('active');
+      break;
+    case 'inscription':
+      $('.nav__link-inscription').attr('tabindex', -1);
+      $('.nav__link-inscription').addClass('active');
+      break;
+    case 'bibliography':
+      $('.nav__link-bibliography').attr('tabindex', -1);
+      $('.nav__link-bibliography').addClass('active');
+      break;
+    default:
+      $('.nav__link-home').attr('tabindex', -1);
+      $('.nav__link-brand').attr('tabindex', -1);
+      $('.nav__link-home').addClass('active');
+      $('.nav__link-brand').addClass('active');
+      break;
+  }
 
 
 
-    /* Inscription */
+
+
+  /**
+  * Article 
+  */ 
+  if($('.about').length) {
+    //Map
+    createMap('map-location', 41.750306229121676, 1.8436313269920916, '', '', 'Club de tenis', 15);
+
+    //Calendar
+    createApp(Calendar).use().mount('#calendar');
+
+    //Tournament
+    createApp(Tournament).use().mount('#tournament');
+  }
+
+
+
+
+
+  /**
+   * Inscription 
+   */
+  if($('.inscription__form').length) {
+    //Change value of progress bar when input is valid    
+    $('input').on('keyup', function() {
+      progressForm();
+    });
+    
+
     //Allow only press key numbers
     $('.input-number').on('keydown', function (e) { 
-      if ((e.which < 48 || e.which > 57) && (e.which != 8) && (e.which < 37 || e.which > 40) && (e.which != 46)) {
+      if ((e.which < 48 || e.which > 57) && (e.which != 8) && (e.which < 37 || e.which > 40) && (e.which != 46) && (e.which != 9) && (e.which < 96 || e.which > 105)) {
           e.preventDefault();
       }
     });     
@@ -80,7 +101,7 @@ import Calendar from './../../views/about/Calendar.vue';
     $('.step').first().show();
 
     //When select the category the list of corresponding levels is displayed.
-    $('#category').on('click', function() {
+    $('#category').on('change', function() {
         let formCateogrySelected = $('#category').val();
         $('.level').hide();
         $('.level-'+formCateogrySelected).show();
@@ -89,13 +110,13 @@ import Calendar from './../../views/about/Calendar.vue';
     stepShow();
 
     //Show prev step
-    $('#prev').on('click', function() {
+    $('#prev').on('click tap touchstart', function() {
         $('.step:visible').hide().prev().show();
         stepShow();
     });
 
     //Show next step after confirm all inputs are valid
-    $('#next').on('click', function() {
+    $('#next').on('click tap touchstart', function() {
       let name = $('.inscription__form__personal #name').is(':invalid');
       let surname = $('.inscription__form__personal #surname').is(':invalid');
       let age = $('.inscription__form__personal #age').is(':invalid');
@@ -115,7 +136,7 @@ import Calendar from './../../views/about/Calendar.vue';
                 zip === false
       );
     });
-    
+  
     //Validate last step 
     $('#inscription__form').on('submit', function(e) {
       let nameClub = $('.inscription__form__professional #name-club').is(':invalid');
@@ -147,9 +168,17 @@ import Calendar from './../../views/about/Calendar.vue';
       }
       this.classList.add('was-validated');
     });
-
+  }
 } )();
 
+
+
+
+
+
+/**
+ * Functions
+ */
 //Create map and marker
 function createMap(el, latitude, altitude, img, alt, title, zoom){
     const mapOptions = {
@@ -175,6 +204,36 @@ function createMap(el, latitude, altitude, img, alt, title, zoom){
     }
     marker.addTo(map);
 }
+
+
+
+//Change value of progress bar when input is valid
+function progressForm() {
+  const progressBar = $('.progress__status');
+  let progressStatus = 4;    
+  
+  const valuesArray = ['#name', '#surname', '#age', '#phone', '#email', '#address-personal', '#city-personal', '#zip-personal',
+                        '#name-club', '#phone-club', '#email-club', '#address-club', '#city-club', '#zip-club', '#category']
+
+  let category = $('.inscription__form__professional #category').is(':invalid');
+  let level = $('.inscription__form__professional input[name="level"').length === 0;
+
+  for (const value of valuesArray) {
+    if( $(value).is(':invalid') === false) {
+      progressStatus = progressStatus + 6;
+      progressBar.css('width', `${progressStatus}%`);
+      progressBar.html(`${progressStatus}%`);
+    }
+  }
+  
+  if (($('input[name="level"').length === 0) === false) {
+    progressStatus = progressStatus + 6;
+    progressBar.css('width', `${progressStatus}%`);
+    progressBar.html(`${progressStatus}%`);
+  }
+}
+
+
 
 //Show, hide and disable buttons Prev, Next and Submit.
 function stepShow () {
---
+++ src/views/about/Tournament.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="bracket">
+        <template v-for="(round, index) in rounds" :key="index">
+            <div class="round" :class="['round-' + index]">
+                <div v-if="round === 32" class="match">
+                    <template  v-for="(player, matchIndex) in players_32" :key="matchIndex">
+                        <div class="match__container">
+                            <div class="match__container__content">{{player.id}}. {{player.name}}</div>
+                        </div>
+                    </template>
+                </div>
+
+                <div v-if="round === 16" class="match">
+                    <template  v-for="(player, matchIndex) in players_16" :key="matchIndex">
+                        <div class="match__container">
+                            <div class="match__container__content">{{player.name}}</div>
+                        </div>
+                    </template>
+                </div>
+
+                <div v-if="round === 8" class="match">
+                    <template  v-for="(player, matchIndex) in players_8" :key="matchIndex">
+                        <div class="match__container">
+                            <div class="match__container__content">{{player.name}}</div>
+                        </div>
+                    </template>
+                </div>
+
+                <div v-if="round === 4" class="match">
+                    <template  v-for="(player, matchIndex) in players_4" :key="matchIndex">
+                        <div class="match__container">
+                            <div class="match__container__content">{{player.name}}</div>
+                        </div>
+                    </template>
+                </div>
+
+                <div v-if="round === 2" class="match">
+                    <template  v-for="(player, matchIndex) in players_2" :key="matchIndex">
+                        <div class="match__container">
+                            <div class="match__container__content">{{player.name}}</div>
+                        </div>
+                    </template>
+                </div>
+
+                <div v-if="round === 1" class="match">
+                    <template  v-for="(player, matchIndex) in players_1" :key="matchIndex">
+                        <div class="match__container">
+                            <div class="match__container__content">{{player.name}}</div>
+                        </div>
+                    </template>
+                </div>
+            </div>
+        </template>
+    </div>
+</template>
+<script>
+import { computed } from 'vue';
+import * as playersJson from './../../assets/json/players.json';
+
+export default {
+    setup() {
+        const defaultRounds = [256, 128, 64, 32, 16, 8, 4, 2, 1];
+        const players = 32;
+        const players_32 = playersJson['32'];
+        const players_16 = playersJson['16'];
+        const players_8 = playersJson['8'];
+        const players_4 = playersJson['4'];
+        const players_2 = playersJson['2'];
+        const players_1 = playersJson['1'];
+
+        let rounds = computed(() => {
+            return defaultRounds.filter(rounds => rounds <= players);
+        });
+
+        return { rounds, players_32, players_16, players_8, players_4, players_2, players_1 }
+    }
+  }
+</script>
+
+
